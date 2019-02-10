73127d30047d4b97cd599c3d7d324ca89899952a
Fardeen Khan <khanfardeen9983@gmail.com>
2019-02-10 19:01:17 +0530

    Added dummy commands to read, add, remove and list all nodes

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
+const yargs = require("yargs")
 
+const note = require('./note') 
+
+var argv = yargs.argv;
+var command = argv._[0]
+switch (command) {
+    case "read":
+        note.readNote(argv.title)
+        break
+    case "add":
+        note.addNote(argv.title,argv.body)
+        break
+    case "remove":
+        note.remove(argv.title)
+        break
+    case "list":
+        note.fetchNotes()
+        break
+    default:
+        console.log("Unknown command")
+}
---
+++ note.js
@@ -0,0 +1,25 @@
+const fs = require('fs')
+
+var addNote = (title, body) => {
+
+}
+
+var remove = (title) => {
+
+}
+
+var readNote = (title) => {
+
+}
+
+var fetchNotes = () => {
+
+}
+
+var saveNotes = (notes) => {
+
+}
+
+module.exports = {
+    addNote, fetchNotes, readNote, remove
+}
